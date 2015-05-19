130a1dc6fea4662001443a4b34879fb5fdb49335
Jernej Virag <jernej@virag.si>
2015-05-19 23:25:27 +0200

    Updated description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='robogif',
       version='1.0',
       description='Simple Android screen recorder',
-      long_description='RoboGif allows you to simply record the screen of your 4.4+ Android device and then convert the recording to high-quality GIF or video.',
+      long_description='RoboGif allows you to simply record the screen of your 4.4+ Android device and then convert the recording to high-quality GIF or video.\n Requires adb and ffmpeg to be installed on the system.',
       author='Jernej Virag',
       license="Apache",
       author_email='jernej@virag.si',
